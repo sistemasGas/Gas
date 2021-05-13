0b9cbfa5997ee938fbb0e86a5f0153e847e9cb9a
edivan <edivan@notebook.localhost>
2021-05-13 01:45:59 -0300

    Adicionado o buscar por ID pessoa.

--- src/main/java/br/com/senai/gasolineapi/resource/PessoaResource.java
+++ src/main/java/br/com/senai/gasolineapi/resource/PessoaResource.java
@@ -1,6 +1,7 @@
 package br.com.senai.gasolineapi.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,7 +16,7 @@ import br.com.senai.gasolineapi.model.Pessoa;
 import br.com.senai.gasolineapi.repository.PessoaRepository;
 
 @RestController
-@RequestMapping("/pessoas")
+@RequestMapping("/pessoa")
 public class PessoaResource {
 
 	@Autowired
@@ -25,11 +26,18 @@ public class PessoaResource {
 	public List<Pessoa> get() {
 		return pessoaRespository.findAll();
 	}
+	
+	@GetMapping("/{id}")
+	public Optional<Pessoa> get(@PathVariable("id") Long id) {
+		return pessoaRespository.findById(id);
+	}
 
 	@PostMapping
 	public void post(@RequestBody Pessoa pessoa) {
 		pessoaRespository.save(pessoa);
 	}
+	
+	
 
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable("id") Long id) {
