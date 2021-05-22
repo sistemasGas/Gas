5428141a7721de9be3925498417678ece98b9004
Jonatha Freitas <jny17@msn.com>
2021-05-22 12:22:13 -0300

    add funcionalidade dashboard qnt cliente

--- src/main/java/br/com/senai/gasolineapi/resource/PessoaResource.java
+++ src/main/java/br/com/senai/gasolineapi/resource/PessoaResource.java
@@ -13,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.senai.gasolineapi.model.Pessoa;
+import br.com.senai.gasolineapi.model.Produto;
 import br.com.senai.gasolineapi.repository.PessoaRepository;
+import br.com.senai.gasolineapi.repository.filter.ProdutoFilter;
+import br.com.senai.gasolineapi.util.PessoaCategoria;
 
 @RestController
 @RequestMapping("/pessoa")
@@ -26,7 +29,7 @@ public class PessoaResource {
 	public List<Pessoa> get() {
 		return pessoaRespository.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
 	public Optional<Pessoa> get(@PathVariable("id") Long id) {
 		return pessoaRespository.findById(id);
@@ -36,12 +39,21 @@ public class PessoaResource {
 	public void post(@RequestBody Pessoa pessoa) {
 		pessoaRespository.save(pessoa);
 	}
-	
-	
 
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable("id") Long id) {
 		pessoaRespository.deleteById(id);
 	}
 
+	@GetMapping("/qntClientes")
+	public int buscaTotalClientes() {
+		List<Pessoa> lista = get();
+
+		int qntCliente = 0;
+		for (Pessoa p : lista) {
+			if (p.getCategoria().equals(PessoaCategoria.CLIFOR))
+				qntCliente++;
+		}
+		return qntCliente;
+	}
 }
