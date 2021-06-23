fe9877a86e8be9b8bf5333c0e17e2906bc69282f
Anthonny Max <anthonnyvall11@gmail.com>
2021-06-23 09:13:42 -0300

    teste login

--- src/test/java/br/com/senai/gasolineapi/GasolineApiApplicationTests.java
+++ src/test/java/br/com/senai/gasolineapi/GasolineApiApplicationTests.java
@@ -4,11 +4,17 @@ package br.com.senai.gasolineapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class GasolineApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testeFindLoginSenha() {
+		
 	}
 
 }
